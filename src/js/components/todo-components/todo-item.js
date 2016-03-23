1761cfe85bad8b1e1af75ffa7bdb80d50c57877b
Michal Sawicki <michal@mobilewebpro.pl>
2016-03-23 12:42:28 +0100

    another todo component - todo-item

---
+++ src/js/components/todo-components/todo-item.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import AppActions from '../../actions/app-actions';
+import TodoTextInput from './todo-textinput';
+import classNames from 'classnames';
+
+/*Co z tymi propTypes trzeba sie dowiedziec ale to pozniej*/
+
+/*var ReactPropTypes = React.PropTypes;*/
+
+/*propTypes: {
+   todo: ReactPropTypes.object.isRequired
+  },*/
+
+
+
+  //Czy te funkcje są w porządku, czy this.props jest dobrze użyte etc. etc.
+
+export default class TodoItem extends React.Component  {
+	constructor(props) {
+	    super(props)
+
+		    this.state = {
+		   	isEditing : false
+	    };
+
+	    this.state._onToggleComplete = this.state._onToggleComplete.bind(this);
+	    this.state._onDoubleClick =this.state._onDoubleClick.bind(this);
+	    this.state._onDestroyClick = this.state._onDestroyClick.bind(this);
+	    this.state._onSave = this.state._onSave.bind(this);
+	}
+	_onToggleComplete() {
+		AppActions.toggleComplete(this.props.todo);
+	},
+	_onDoubleClick() {
+		this.setState({ isEditing: true });
+	},
+	_onSave( text ) {
+		AppActions.updateText(this.props.todo.id, text);
+		this.setState({isEditing: false });
+	}
+	_onDestroyClick(){
+		AppActions.destroy(this.props.todo.id);
+	}
+	render() {
+		return (
+			<li
+			className={classNames({
+				'completed': todo.complete,
+				'editing': this.state.isEditing
+			})}
+			key={todo.id}>
+				<div className="view"?
+					<input
+						className="toggle"
+						type="checkbox"
+						checked={todo.complete}
+						onChange={this.state._onToggleComplete}
+						/>
+						<label onDoubleClick={this.state._onDoubleClick}>
+							{todo.text}
+						</label>
+						<button className="destroy" onClick={this.state._onDestroyClick} />
+				</div>
+				{input}
+			</li>
+
+		);
+	}
+}
